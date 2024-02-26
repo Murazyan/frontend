63db728196bc26188d2dec5e857d5e351087055a
murazyan <36574856+Murazyan@users.noreply.github.com>
2024-02-26 18:27:55 +0400

    finish todo app

--- toDOList/todo.js
+++ toDOList/todo.js
@@ -12,7 +12,7 @@ PageHeader.prototype.getTemplate = function (){
     return headerDiv;
 }
 
-function CustomButton(title, clickCallBack){
+function CustomButton(title, clickCallBack, attibutes){
     this.title=title;
     this.clickCallBack=clickCallBack;
     this.template=this.getTemplate();
@@ -38,6 +38,69 @@ function TableList(parentNode, colNames, dataSorce){
     this.template = this.getTemplate();
 }
 
+TableList.prototype.existRow = function (rowData){
+    let exist = this.dataSorce.filter(row=>{
+        let found = true;
+        for (const [key, value] of Object.entries(rowData)){
+            if(row[key] !== value){
+                found =  false;
+            }
+        }
+        return found;
+    })
+    return exist && exist.length > 0;
+}
+
+TableList.prototype.addRow = function (rowData){
+    if(!this.existRow(rowData)){
+        this.dataSorce.push(rowData)
+        let tbody = document.getElementById('tbody')
+        const row = document.createElement('tr');
+        this.colNames.forEach(colName=>{
+            const column = document.createElement('td');
+            if (colName.render) {
+                const renderedButtons = colName.render(rowData, row);
+                renderedButtons.forEach(b => {
+                    column.appendChild(b.template);
+                })
+            } else {
+                column.innerHTML = rowData[colName.name];
+            }
+            row.appendChild(column);
+            tbody.appendChild(row);
+        })
+        return true;
+    }
+    return false;
+}
+
+TableList.prototype.editRow = function (oldRowData, newRowData, rowNode){
+    if(JSON.stringify(oldRowData)==JSON.stringify(newRowData)){
+        return true;
+    }
+    if(!this.existRow(newRowData)) {
+        Object.keys(oldRowData).forEach(key => {
+            oldRowData[key] = newRowData[key];
+        })
+        let childrens =  rowNode.children;
+        Object.values(newRowData).forEach((value, i)=>{
+            childrens.item(i).innerHTML=value;
+        })
+        return true;
+    }
+    return false;
+
+}
+
+
+TableList.prototype.deleteRow = function (rowData, rowNode){
+   let index = this.dataSorce.indexOf(rowData);
+    this.dataSorce.splice(index, 1);
+    rowNode.remove();
+
+}
+
+
 TableList.prototype.getTemplate = function (){
     if(this.template) return this.template;
     const table = document.createElement('table');
@@ -54,6 +117,7 @@ TableList.prototype.getTemplate = function (){
 
     //    table body
     const tableBody = document.createElement('tbody');
+    tableBody.setAttribute('id', 'tbody')
     this.dataSorce.forEach(dataObj=>{
         const row = document.createElement('tr');
         this.colNames.forEach(colName=>{
@@ -79,6 +143,8 @@ function Modal(title, childNodeObj){
     this.title = title;
     this.childNode = childNodeObj.template;
     this.childNodeObj = childNodeObj;
+    this.errorNode = this.getErrorNode();
+    this.closeBtn = this.getCloseBtn();
     this.template = this.getTemplate();
     this.show = function(){
         this.template.style.display = "block";
@@ -86,7 +152,7 @@ function Modal(title, childNodeObj){
     this.close =function(){
         this.template.style.display = "none";
     }
-    this.update = function(title, childNodeObj){
+    this.update = function(title, showError, childNodeObj){
         this.title= title;
         this.childNode = childNodeObj.template;
         this.childNodeObj = childNodeObj;
@@ -95,44 +161,94 @@ function Modal(title, childNodeObj){
         titileNode.innerHTML = this.title;
         content.innerHTML= '';
         content.appendChild(titileNode);
+        content.appendChild(this.errorNode);
+        content.insertBefore(this.closeBtn, content.firstChild);
         content.appendChild(this.childNode);
+
+        if(showError){
+            this.showError();
+        }else{
+            this.closeError();
+        }
     }
     this.getModalContent = function(){
         return  this.childNodeObj;
     }
+
 }
+
+Modal.prototype.getCloseBtn = function(){
+    if(this.closeBtn) return this.closeBtn;
+    const closeBtn = document.createElement('close');
+    closeBtn.classList.add("close");
+    closeBtn.innerHTML='&times;';
+    closeBtn.addEventListener('click', (event)=>{
+        event.preventDefault();
+        this.close();
+    })
+    return closeBtn;
+}
+
+Modal.prototype.getErrorNode = function(){
+    if(this.errorNode) return this.errorNode;
+    const errorDiv = document.createElement('div');
+    errorDiv.setAttribute('id', 'modal-error');
+    errorDiv.classList.add('modal-error');
+    const errorText = document.createElement('span');
+    errorText.setAttribute('id', 'modal-error-text');
+    errorDiv.appendChild(errorText)
+    return errorDiv;
+}
+
 
 Modal.prototype.getTemplate = function(){
     if(this.template) return this.template;
     const modal = document.createElement('div');
     modal.classList.add('modal');
+
     const modalContent = document.createElement('div');
     modalContent.classList.add('modal-content');
     modalContent.setAttribute('id', 'modal-content');
     const modalTitle = document.createElement('span');
     modalTitle.setAttribute('id', 'modal-title');
     modalTitle.innerHTML=this.title;
-    const closeBtn = document.createElement('close');
-    closeBtn.classList.add("close");
-    closeBtn.innerHTML='&times;';
+  
     modalTitle.innerHTML=this.title;
     modalContent.appendChild(modalTitle);
+    modalContent.appendChild(this.errorNode)
     modalContent.appendChild(this.childNode)
-    modal.appendChild(closeBtn);
+    modalContent.insertBefore(this.closeBtn, modalContent.firstChild);
     modal.appendChild(modalContent);
-    return modal
+    return modal;
 }
 
+Modal.prototype.showError = function(errorText){
+    let div = document.getElementById('modal-error')
+    let span = document.getElementById('modal-error-text');
+    span.innerHTML = errorText;
+    div.classList.add("modal-error-show");
+    div.classList.remove("modal-error");
+
+}
+
+Modal.prototype.closeError = function(errorText){
+    let div = document.getElementById('modal-error')
+    div.classList.remove("modal-error-show");
+    div.classList.add("modal-error");
+}
 //inputsInfo [{inputLable: inputLable, inputType: type, inputValue: value}]
 
-function ModalContent(inputsInfo, buttons){
+function ModalContent(inputsInfo, buttons, formAttributes){
     this.inputsInfo = inputsInfo;
     this.buttons = buttons;
+    this.formAttributes = formAttributes;
     this.inputsMap = new Map();
     this.template = this.getTemplate();
 
     this.getData = function(){
-        return  Array.from(this.inputsMap, ([name, val]) => ({[name]: val.value}));
+        let model = {};
+        this.inputsMap.forEach((val, name )=>model[name]=val.value);
+        return model;
     }
 }
 
@@ -157,6 +273,9 @@ ModalContent.prototype.getTemplate = function(){
             buttonsContainer.appendChild(button.template);
         })
         form.appendChild(buttonsContainer);
+    }
+    if(this.formAttributes){
+        this.formAttributes.forEach(atr=>form.setAttribute(atr, atr))
     }
 
     return form;
@@ -186,21 +305,41 @@ const run =  ()=>{
             render: (rowData, row) => {
                 return [
                     new CustomButton('Edit', ()=>{
-                        alert("edit row")
-                        console.log(row, rowData)
+                        modal.update("Edit item", false,
+                            new ModalContent(tableColNames.filter(col=>!col.render).map(col=>{return {inputLable: col.name, inputType: 'text', inputValue: rowData[col.name]}}),
+                                [
+                                    new CustomButton('Cancle', (event)=>{
+                                        event.preventDefault();
+                                        modal.close();
+                                    }),
+                                    new CustomButton("Edit", (event)=>{
+                                        event.preventDefault();
+                                        const newRowData = modal.getModalContent().getData()
+                                        if(tableList.editRow(rowData, newRowData, row)){
+                                            modal.close();
+                                        }else{
+                                            modal.showError("Record already exists in the table")
+                                        }
+                                    })]));
+
+                        modal.show()
                     }),
                     new CustomButton('Delete', ()=>{
-                        alert("Delete row")
+                        tableList.deleteRow(rowData, row);
                     }),
                     new CustomButton('Show', ()=>{
-                        alert("Show row")
+                        modal.update("Element information", false,
+                        new ModalContent(tableColNames.filter(col=>!col.render)
+                        .map(col=>{return {inputLable: col.name, inputType: 'text', inputValue: rowData[col.name]}}), [], ['disabled']));
+
+                    modal.show()
                     })]
             }
         }];
     modal =  new Modal('', new ModalContent(tableColNames.filter(col=>!col.render).map(col=>{return {inputLable: col.name, inputType: 'text', inputValue: ''}})));
     document.body.appendChild(modal.template);
     const addRowBtn = new CustomButton('Add', ()=>{
-        modal.update("Add new item",
+        modal.update("Add new item", false,
             new ModalContent(tableColNames.filter(col=>!col.render).map(col=>{return {inputLable: col.name, inputType: 'text', inputValue: ''}}),
                 [new CustomButton('Cancle', (event)=>{
                     event.preventDefault();
@@ -208,8 +347,14 @@ const run =  ()=>{
                 }),
                     new CustomButton("Add", (event)=>{
                         event.preventDefault();
-                        modal.close();
-                        modal.getModalContent().getData();
+                        const rowData = modal.getModalContent().getData()
+                        if(tableList.addRow(rowData)){
+                            modal.close();
+                        }else{
+                            modal.showError("Record already exists in the table")
+                            console.log('error')
+                        }
+                        console.log('data {}', modal.getModalContent().getData())
                     })]));
 
         modal.show()
@@ -227,19 +372,3 @@ const run =  ()=>{
 
 };
 run()
-
-for(var i = 0; i<10; ++i){
-    setT(i);
-}
-
-function setT(i){
-    setTimeout(()=>{
-        console.log(i+"");
-    }, 1000);
-}
-
-for(var i = 0; i<10; ++i){
-    setTimeout(()=>{
-        console.log(i);
-    }, 1000);
-}
